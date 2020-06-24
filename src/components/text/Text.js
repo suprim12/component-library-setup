fa466f74635a0f8479d2e72fef9b12d2e59c013d
suprim12 <suprimofficial@yahoo.com>
2020-06-24 12:24:12 +0600

    Test & Lint Phase

--- src/components/text/Text.js
+++ src/components/text/Text.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
-const Text = () => {
-    return (
-        <div>
-
-        </div>
-    )
+const Text = ({ children }) => {
+  return <div>{children}</div>
 }
 
 export default Text
--- src/tests/Text.test.js
+++ src/tests/Text.test.js
@@ -1,1 +1,8 @@
-import Text from '../components/text/Text';
+import React from 'react'
+import Text from '../components/text/Text'
+import { render } from '@testing-library/react'
+
+test('renders text component correctly', () => {
+  const div = document.createElement('div')
+  render(<Text />, div)
+})
